754ebbe7d1f736423aead2516eac53169ccb497e
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2026-02-19 15:18:16 +0000

    refactor: simplify connection handling by removing unnecessary ACK message processing

--- 02-cloud-native-tooling/golang/l4-loadbalancer/main.go
+++ 02-cloud-native-tooling/golang/l4-loadbalancer/main.go
@@ -1,40 +1,32 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
-	"strings"
 )
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	reader := bufio.NewReader(conn)
-	message, err := reader.ReadString('\n')
+	connDial, errDial := net.Dial("tcp", "localhost:9999")
 
-	if err != nil {
-		log.Fatal("Read error: ", err)
-		return
-	}
-	ackMsg := strings.TrimSpace(message)
-	response := fmt.Sprintf("New ACK message:%s\n", ackMsg)
-
-	conn_dial, err_dial := net.Dial("tcp", "localhost:9999")
-
-	if err_dial != nil {
+	if errDial != nil {
 		log.Fatal("Error while initializing the dial to localhost:9999")
 	}
-	conn_dial.Write([]byte(response))
 
+	go io.Copy(conn, connDial)
+	io.Copy(connDial, conn)
 }
 
 func main() {
 	ln, err := net.Listen("tcp", ":8888")
 	if err != nil {
 		log.Fatal("Error listening:", err)
+		return
 	}
+	fmt.Println("Listening on port 8888")
 
 	for {
 		conn, err := ln.Accept()
