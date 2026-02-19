8d9dc5e2f6342e9568d9d1b948717cae7e189ec0
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2026-02-19 15:06:33 +0000

    feat: implement TCP load balancer with connection handling

---
+++ 02-cloud-native-tooling/golang/l4-loadbalancer/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	message, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Fatal("Read error: ", err)
+		return
+	}
+	ackMsg := strings.ToUpper(strings.TrimSpace(message))
+	response := fmt.Sprintf("ACK %s\n", ackMsg)
+
+	if err != nil {
+		log.Fatal("Server write error: ", err)
+	}
+	conn_dial, err_dial := net.Dial("tcp", "localhost:9999")
+
+	if err_dial != nil {
+		log.Fatal("Error while initializing the dial to localhost:9999")
+	}
+	conn_dial.Write([]byte(response))
+
+}
+
+func main() {
+	ln, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		log.Fatal("Error listening:", err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal("Error accepting connection: ", err)
+		}
+		go handleConnection(conn)
+	}
+
+}
