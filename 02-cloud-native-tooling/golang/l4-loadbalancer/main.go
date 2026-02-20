9cc1b6dacded6cf9d4266070f68f20d504a0fe49
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2026-02-20 14:31:43 +0000

    feat: enhance connection handling by implementing backend selection for load balancing

--- 02-cloud-native-tooling/golang/l4-loadbalancer/main.go
+++ 02-cloud-native-tooling/golang/l4-loadbalancer/main.go
@@ -5,35 +5,55 @@ import (
 	"io"
 	"log"
 	"net"
+	"strings"
 )
 
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
+func handleConnection(conn net.Conn, backend string) {
 
-	connDial, errDial := net.Dial("tcp", "localhost:9999")
+	connDial, errDial := net.Dial("tcp", backend)
 
 	if errDial != nil {
-		log.Fatal("Error while initializing the dial to localhost:9999")
+		log.Println("Erro no Dial:", errDial)
+		return
 	}
 
-	go io.Copy(conn, connDial)
-	io.Copy(connDial, conn)
+	defer conn.Close()
+	defer connDial.Close()
+
+	done := make(chan bool)
+	go func() {
+		io.Copy(conn, connDial)
+		done <- true
+	}()
+	go func() {
+		io.Copy(connDial, conn)
+		done <- true
+	}()
+	<-done
+	<-done
+
 }
 
 func main() {
-	ln, err := net.Listen("tcp", ":8888")
+	listenPort := ":8888"
+	ln, err := net.Listen("tcp", listenPort)
 	if err != nil {
 		log.Fatal("Error listening:", err)
 		return
 	}
-	fmt.Println("Listening on port 8888")
+	fmt.Printf("Listening on port %v", strings.Split(listenPort, ":")[1])
 
+	backends := []string{"localhost:9997", "localhost:9998", "localhost:9999"}
+
+	counter := 0
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			log.Fatal("Error accepting connection: ", err)
 		}
-		go handleConnection(conn)
+		target := backends[counter%len(backends)]
+		go handleConnection(conn, target)
+		counter++
 	}
 
 }
