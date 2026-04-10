9674b0ae971038c2c87346976e5b21ede0b4a880
Diogo Guedes <diogo.guedes111@gmail.com>
2026-04-10 12:07:57 +0100

    feat: optimize conversation history handling by limiting messages to the last 10

--- 03-data-ai-engineering/agentic-ai/agent.py
+++ 03-data-ai-engineering/agentic-ai/agent.py
@@ -5,12 +5,16 @@ client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
 history = []
 history.append(" O agente deve falar em Portugues de portugal, se muito suncinto e pratico.\n")
 # LLM does not have memory. We need to provide the history of the conversation in each request.
+
+MAX_MESSAGES=10
+
 while True:
     user_input = input("You: ")
     history.append(user_input)
+    window = history[-MAX_MESSAGES:]
     response = client.models.generate_content(
         model="gemini-2.5-flash-lite",
-        contents=history
+        contents=window
     )
     reply = response.text
     history.append(reply)
