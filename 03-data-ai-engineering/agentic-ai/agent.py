c205f73ae6b6c88bd9e1d91a0223c88d4c76dacf
Diogo Guedes <diogo.guedes111@gmail.com>
2026-04-11 14:56:01 +0100

    feat: refactor conversation handling to summarize history when exceeding message limit

--- 03-data-ai-engineering/agentic-ai/agent.py
+++ 03-data-ai-engineering/agentic-ai/agent.py
@@ -8,13 +8,25 @@ history.append(" O agente deve falar em Portugues de portugal, se muito suncinto
 
 MAX_MESSAGES=10
 
-while True:
-    user_input = input("You: ")
-    history.append(user_input)
-    window = history[-MAX_MESSAGES:]
+def summarize(history):
+
+    prompt = "Summarize the following conversation:\n" + "\n".join(history)
     response = client.models.generate_content(
         model="gemini-2.5-flash-lite",
-        contents=window
+        contents=[prompt]
+    )
+    return response.text
+
+while True:
+    user_input = input("You: \n")
+    history.append(user_input)
+
+    if len(history) > MAX_MESSAGES:
+        summary = summarize(history)
+        history = [summary]
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-lite",
+        contents=history
     )
     reply = response.text
     history.append(reply)
