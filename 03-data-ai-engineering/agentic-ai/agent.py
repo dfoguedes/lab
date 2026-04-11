429309dec5474a8857be35d450a187688973e254
Diogo Guedes <diogo.guedes111@gmail.com>
2026-04-11 15:26:42 +0100

    feat: enhance agent functionality with current time tool and improved response handling (tool calls)

--- 03-data-ai-engineering/agentic-ai/agent.py
+++ 03-data-ai-engineering/agentic-ai/agent.py
@@ -1,5 +1,7 @@
 from google import genai
+from google.genai import types
 import os
+import datetime
 client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
 
 history = []
@@ -17,8 +19,24 @@ def summarize(history):
     )
     return response.text
 
+def get_current_time():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+tools = [types.Tool(
+    function_declarations=[{
+        "name": "get_current_time",
+        "description": "Returns the current date and time in the format YYYY-MM-DD HH:MM:SS",
+        "parameters": {
+            "type": "object",
+            "properties": {},
+            "required": []
+        }
+    }]
+
+)]
 while True:
-    user_input = input("You: \n")
+    user_input = input("You: ")
     history.append(user_input)
 
     if len(history) > MAX_MESSAGES:
@@ -26,8 +44,29 @@ while True:
         history = [summary]
     response = client.models.generate_content(
         model="gemini-2.5-flash-lite",
-        contents=history
+        contents=history,
+        config=types.GenerateContentConfig(tools=tools)
     )
+
+    if response.candidates[0].content.parts[0].function_call:
+        if (response.candidates and 
+            response.candidates[0].content and 
+            response.candidates[0].content.parts and
+            response.candidates[0].content.parts[0].function_call):
+            tool_call = response.candidates[0].content.parts[0].function_call
+        if tool_call.name == "get_current_time":
+            result = get_current_time()
+            history.append(f"Tool call: {tool_call.name} with result: {result}")
+            response = client.models.generate_content(
+                    model="gemini-2.5-flash-lite",
+                    contents=history,
+                    config=types.GenerateContentConfig(tools=tools)
+                )
+        else: 
+            history.append("Tool call:" + tool_call.name + " not found")
+            break
+            
+    
     reply = response.text
     history.append(reply)
-    print(f"Gemini: {reply}")
+    print(f"\nGemini: {reply}")
