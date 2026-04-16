82800309a7cee93447a394df6d1bd1dbe396ec0c
Diogo Guedes <diogo.guedes111@gmail.com>
2026-04-16 12:38:49 +0100

    feat: enhance SYSTEM_INSTRUCTION and expand URL list for news feed summarization

--- 03-data-ai-engineering/news-feeds/news.py
+++ 03-data-ai-engineering/news-feeds/news.py
@@ -5,15 +5,33 @@ import os
 import datetime
 
 client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
-SYSTEM_INSTRUCTION="A resposta deve ser sempre em Portugues de Portugal. e deves agrupar por categoria formatar com emojis e identificar apenas aqueles que forem de grande relevancia para ler e no limite de 5 noticias. A resposta deve ser formatada em markdown e deve conter um titulo para cada categoria. A resposta deve conter um resumo de cada noticia e o link para ler a noticia completa."
+SYSTEM_INSTRUCTION="A resposta deve ser sempre em Portugues de Portugal. " \
+"deves agrupar por categoria, formatar com emojis" \
+" identificar apenas aqueles que forem de grande importancia" \
+" e no limite de 5 noticias. " \
+"A resposta deve ser formatada em markdown " \
+"e deve conter um titulo para cada categoria. " \
+"A resposta deve conter um resumo de cada noticia " \
+"e o link para ler a noticia completa."
 
 
-URLS = ["https://observador.pt/feed/","https://www.rtp.pt/noticias/rss/","https://shifter.pt/feed/"]
+URLS = ["https://observador.pt/feed/",
+        "https://www.rtp.pt/noticias/rss/",
+        "https://shifter.pt/feed/",
+        "https://feeds.arstechnica.com/arstechnica/index",
+        "https://www.theverge.com/rss/index.xml",
+        "https://techcrunch.com/feed/",
+        "https://feeds.feedburner.com/TheHackersNews",
+        "https://hnrss.org/frontpage",
+        "https://www.wired.com/feed/rss",
+        "https://pplware.sapo.pt/feed/",
+        "https://abertoithoje.pt/feed/",
+        ]
 
 def summarize(entries):
     prompts = []
     for entry in entries:
-        prompt = f"Title: {entry.title} , Summary: {entry.summary}"
+        prompt = f"Title: {entry.title} , Summary: {entry.summary}, Link: {entry.link}"
         prompts.append(prompt)
 
     response = client.models.generate_content(
